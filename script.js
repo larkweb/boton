5281365200e56e995bfade072d038d97d79444a9
larkweb <148163767+larkweb@users.noreply.github.com>
2024-03-21 14:33:07 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("redirectButton").addEventListener("click", function() {
+    window.location.href = "https://larkweb.github.io/flower/"; // Cambia "https://www.ejemplo.com" por la URL a la que quieres redirigir
+  });
